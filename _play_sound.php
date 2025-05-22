0e165f0804cbc625fde0e94ddf7505f5de05667e
Ben Servoz <benjamin.servoz@dartmouth.edu>
2025-05-22 12:19:42 -0400

    adjusted Pi volume to avoid saturation

--- _play_sound.php
+++ _play_sound.php
@@ -9,7 +9,7 @@ if( !isset($argv[1]) ) {
 $filename = "/dev/shm/" . md5($argv[1]) . "-" . date( microtime(true) ) . ".wav" ;
 
 shell_exec( "/usr/bin/pico2wave -w {$filename} \"{$argv[1]}\"" ) ;
-shell_exec( "/usr/bin/amixer set Headphone 91%" ) ;
+shell_exec( "/usr/bin/amixer set PCM 85%" ) ;
 shell_exec( "/usr/bin/sudo /usr/bin/play {$filename}" ) ;
 unlink( $filename ) ;
 
